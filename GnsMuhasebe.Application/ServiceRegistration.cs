fef91c75f885d3d6f80d2e6ac8c9ed773e311fda
Necdet Güneş <nmehmetgunes@gmail.com>
2026-01-14 16:14:55 +0300

    AutoMapper ve MediatR paketleri register eylendi

---
+++ GnsMuhasebe.Application/ServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace GnsMuhasebe.Application
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
+
+            services.AddAutoMapper(cfg => { cfg.AddMaps(assembly); });
+        }
+    }
+}
--- Güneş Muhasebe/Program.cs
+++ Güneş Muhasebe/Program.cs
@@ -1,3 +1,4 @@
+using GnsMuhasebe.Application;
 using GnsMuhasebe.Infrastucture.Persistance;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(connectionString,b => b.MigrationsAssembly("GnsMuhasebe.Infrastucture")));
